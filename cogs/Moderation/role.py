291391608f0b6c2b8d637693be63030b9b2881cc
Sarthak Aggarwal <70897806+aggarwal-sarthak@users.noreply.github.com>
2023-06-28 09:08:35 +0530

    Update role.py

--- cogs/Moderation/role.py
+++ cogs/Moderation/role.py
@@ -15,26 +15,41 @@ class role(commands.Cog):
             case 'add':
                 member_list=[]
                 role_list=[]
+                member_string=""
+                role_string=""
+
                 for id in ids:
                     member = ctx.guild.get_member(int(id))
                     role = ctx.guild.get_role(int(id))
                     if role:
-                        if ctx.guild.get_member(self.client.user.id).top_role.position <= role.position:
-                            await ctx.reply(f"{self.client.emotes['failed']} | My Role Isn't High Enough To Assign The Role `{role.name}`!")
-                        elif ctx.guild.owner_id != ctx.author.id and ctx.author.top_role.position <= role.position:
-                            await ctx.reply(f"{self.client.emotes['failed']} | Your Role Isn't High Enough To Assign The Role `{role.name}`!")
-                        else:
-                            role_list.append(id)
+                        if await position_check(self=self, ctx=ctx, role=role): pass
+                        else: role_list.append(id)
                             
                     if member:
                         member_list.append(id)
-
-                print(role_list, member_list)
+                    
+                await give_role(self=self, ctx=ctx, role_list=role_list, member_list=member_list, role_string=role_string, member_string=member_string)
 
             case 'remove':
                 print(ids)
             case _:
                 print('wrong')
+
+async def position_check(self, ctx, role):
+    if ctx.guild.get_member(self.client.user.id).top_role.position <= role.position:
+        await ctx.reply(f"{self.client.emotes['failed']} | My Role Isn't High Enough To Assign The Role `{role.name}`!")
+    elif ctx.guild.owner_id != ctx.author.id and ctx.author.top_role.position <= role.position:
+        await ctx.reply(f"{self.client.emotes['failed']} | Your Role Isn't High Enough To Assign The Role `{role.name}`!")
+        
+async def give_role(self, ctx, role_list, member_list, role_string, member_string):
+    for r in role_list:
+        for m in member_list:
+            await ctx.guild.get_member(int(m)).add_roles(ctx.guild.get_role(int(r)))
+                
+    for r in role_list: role_string += "`" + ctx.guild.get_role(int(r)).name + "`,"
+    for m in member_list: member_string += "`" + ctx.guild.get_member(int(m)).name + "`,"
+
+    await ctx.reply(f"{self.client.emotes['success']} | Added {role_string} To {member_string}!")
 
 async def setup(client):
     await client.add_cog(role(client))
