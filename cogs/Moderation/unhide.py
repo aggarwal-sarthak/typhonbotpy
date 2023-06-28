20f5d7af6df837e74afd3e863ee86533c3ed86cd
Sarthak Aggarwal <70897806+aggarwal-sarthak@users.noreply.github.com>
2023-06-28 13:25:23 +0530

    Update unhide.py

--- cogs/Moderation/unhide.py
+++ cogs/Moderation/unhide.py
@@ -9,14 +9,16 @@ class unhide(commands.Cog):
     async def on_ready(self):
         print(f"✅ | {os.path.basename(__file__)[:-3]} Is Loaded!")
 
-    @commands.command(description='Unhides Current/Mentioned Channel(s) For Everyone', usage=f"{os.path.basename(__file__)[:-3]}")
+    @commands.command(description='Unhides Current/Mentioned Channel(s) For Everyone', usage=f"{os.path.basename(__file__)[:-3]} [channel]")
     async def unhide(self, ctx):
         channel = ctx.message.channel_mentions
         mentions = ""
         if not channel:
-            channel = ctx.channel
+            channel = [ctx.channel]
         for c in channel:
-            await c.set_permissions(ctx.guild.default_role, view_channel=True)
+            perms = c.overwrites_for(ctx.guild.default_role)
+            perms.view_channel = True
+            await c.set_permissions(ctx.guild.default_role, overwrite=perms)
             mentions += "<#"+str(c.id)+"> " 
         await ctx.send(f'{self.client.emotes["success"]} | {mentions} Is Unhidden!')
 
