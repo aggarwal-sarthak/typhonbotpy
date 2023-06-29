4f45dd2b5ef28316a206fb480bb2a795df2ca224
Sarthak Aggarwal <70897806+aggarwal-sarthak@users.noreply.github.com>
2023-06-29 18:07:24 +0530

    Update role.py

--- cogs/Moderation/role.py
+++ cogs/Moderation/role.py
@@ -13,23 +13,26 @@ class role(commands.Cog):
     @commands.bot_has_permissions(manage_roles=True)
     @commands.command(description='Add/Remove Roles', aliases=['r'], usage=f"{os.path.basename(__file__)[:-3]}")
     async def role(self, ctx, mode, *ids):
+        ids = await parse_ids(ids)
+        member_list=[]
+        role_list=[]
+        member_string=""
+        role_string=""
+
         match mode:
             case 'add':
-                member_list=[]
-                role_list=[]
-                member_string=""
-                role_string=""
-
                 for id in ids:
                     member = ctx.guild.get_member(int(id))
                     role = ctx.guild.get_role(int(id))
+                    
                     if role:
-                        if await position_check(self=self, ctx=ctx, role=role): pass
+                        if await position_check(self=self, ctx=ctx, role=role):
+                            pass
                         else: role_list.append(id)
                             
                     if member:
                         member_list.append(id)
-                    
+
                 await give_role(self=self, ctx=ctx, role_list=role_list, member_list=member_list, role_string=role_string, member_string=member_string)
 
             case 'remove':
@@ -46,7 +49,20 @@ class role(commands.Cog):
         if isinstance(error, commands.BotMissingPermissions):
             err = str(error).replace('Bot requires ','').replace(' permission(s) to run this command.', '')
             await ctx.reply(f"{self.client.emotes['failed']} | I Don't Have `{err}` Permission To Use This Command!")
-            
+
+async def parse_ids(ids):
+    parsed_ids = []
+    for id in ids:
+        if "<@&" in id:
+            parsed_ids.append(id[3:-1])
+
+        elif "<@" in id:
+            parsed_ids.append(id[2:-1])
+        
+        else:
+            parsed_ids.append(id)
+    return parsed_ids
+
 async def position_check(self, ctx, role):
     if ctx.guild.get_member(self.client.user.id).top_role.position <= role.position:
         await ctx.reply(f"{self.client.emotes['failed']} | My Role Isn't High Enough To Assign The Role `{role.name}`!")
@@ -62,8 +78,6 @@ async def give_role(self, ctx, role_list, member_list, role_string, member_strin
     for m in member_list: member_string += "`" + ctx.guild.get_member(int(m)).name + "`,"
 
     await ctx.reply(f"{self.client.emotes['success']} | Added {role_string} To {member_string}!")
-
-
 
 async def setup(client):
     await client.add_cog(role(client))
