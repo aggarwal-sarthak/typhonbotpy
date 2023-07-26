403c4181e5fc0a30d50f6bb57bd37dcba7a7a9c0
ShubhamPokhariyal <shubhampokhariyal2@gmail.com>
2023-07-26 22:05:08 +0530

    created separate file for confirmation

--- cogs/Moderation/ban.py
+++ cogs/Moderation/ban.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import json
-
+import confirmation
 with open('emoji.json', 'r') as f:
     emotes = json.load(f)
 
@@ -19,7 +19,7 @@ class ban(commands.Cog):
     @commands.bot_has_permissions(ban_members=True)
     @commands.command(description="Bans Member from the Server",aliases=['None'],usage=f"{os.path.basename(__file__)[:-3]} <user> []")
     async def ban(self,ctx,user: discord.Member):
-        view = Buttons(ctx)
+        view = confirmation.Buttons(ctx)
         msg = await ctx.reply(f"You are about to ban: {user}",view=view)
         await view.wait()
         if view.value == "1":
@@ -37,24 +37,7 @@ class ban(commands.Cog):
 
 
 
-class Buttons(discord.ui.View):
-    def __init__ (self, ctx, *, timeout=60):
-        super().__init__(timeout=timeout)
-        self.ctx = ctx
-    @discord.ui.button(label="Ban", style=discord.ButtonStyle.green)
-    async def button1_call(self, interaction: discord.Interaction, button: discord.Button):
-        if interaction.user != self.ctx.author:
-            return await interaction.response.send_message(f"{emotes['failed']} | You Cannot Interact With This Button!", ephemeral=True)
-        self.value = "1"
-        self.stop()
-        await interaction.response.defer()
-    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
-    async def button2_call(self, interaction: discord.Interaction, button: discord.Button):
-        if interaction.user != self.ctx.author:
-            return await interaction.response.send_message(f"{emotes['failed']} | You Cannot Interact With This Button!", ephemeral=True)
-        self.value = "2"
-        self.stop()
-        await interaction.response.defer()
+
 
 
 async def setup(client):
---
+++ confirmation.py
@@ -0,0 +1,24 @@
+import discord
+import json
+with open('emoji.json', 'r') as f:
+    emotes = json.load(f)
+
+
+class Buttons(discord.ui.View):
+    def __init__ (self, ctx, *, timeout=60):
+        super().__init__(timeout=timeout)
+        self.ctx = ctx
+    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.green)
+    async def button1_call(self, interaction: discord.Interaction, button: discord.Button):
+        if interaction.user != self.ctx.author:
+            return await interaction.response.send_message(f"{emotes['failed']} | You Cannot Interact With This Button!", ephemeral=True)
+        self.value = "1"
+        self.stop()
+        await interaction.response.defer()
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
+    async def button2_call(self, interaction: discord.Interaction, button: discord.Button):
+        if interaction.user != self.ctx.author:
+            return await interaction.response.send_message(f"{emotes['failed']} | You Cannot Interact With This Button!", ephemeral=True)
+        self.value = "2"
+        self.stop()
+        await interaction.response.defer()
