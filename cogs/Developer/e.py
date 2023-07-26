c793671481b99ce5bedffb68cf7c2e5c531e3872
ShubhamPokhariyal <shubhampokhariyal2@gmail.com>
2023-07-26 21:55:37 +0530

    renamed eval to e

--- cogs/Developer/e.py
+++ cogs/Developer/e.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import os
 
-class eval(commands.Cog):
+class e(commands.Cog):
     def __init__(self, client):
         self.client = client
 
@@ -9,15 +9,16 @@ class eval(commands.Cog):
     async def on_ready(self):
         print(f"✅ | {os.path.basename(__file__)[:-3]} Is Loaded!")
 
-    @commands.command(description='Evaluates given code', usage=f"{os.path.basename(__file__)[:-3]}")
-    async def eval(self, ctx,  *, code: str):
+    @commands.command(description='Evaluates given code',aliases=['eval'], usage=f"{os.path.basename(__file__)[:-3]}")
+    async def e(self, ctx, *,code: str):
         if ctx.author.id not in self.client.config["owner"]: return
         try:
             result = eval(code)
+            
             await ctx.send(f"{self.client.emotes['success']} | Result: {result}")
 
         except Exception as e:
             await ctx.send(f"{self.client.emotes['failed']} | Error: {e}")
-
+       
 async def setup(client):
-    await client.add_cog(eval(client))
+    await client.add_cog(e(client))
