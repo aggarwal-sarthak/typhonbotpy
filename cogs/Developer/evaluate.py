17fbe79441ff513363a2bf21b94d889f22101a08
Sarthak Aggarwal <70897806+aggarwal-sarthak@users.noreply.github.com>
2023-07-26 22:13:37 +0530

    Update evaluate.py

--- cogs/Developer/evaluate.py
+++ cogs/Developer/evaluate.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import os
 
-class e(commands.Cog):
+class evaluate(commands.Cog):
     def __init__(self, client):
         self.client = client
 
@@ -10,7 +10,7 @@ class e(commands.Cog):
         print(f"✅ | {os.path.basename(__file__)[:-3]} Is Loaded!")
 
     @commands.command(description='Evaluates given code',aliases=['eval', 'e'], usage=f"{os.path.basename(__file__)[:-3]}")
-    async def e(self, ctx, *,code: str):
+    async def evaluate(self, ctx, *,code: str):
         if ctx.author.id not in self.client.config["owner"]: return
         try:
             result = eval(code)
@@ -21,4 +21,4 @@ class e(commands.Cog):
             await ctx.send(f"{self.client.emotes['failed']} | Error: {e}")
        
 async def setup(client):
-    await client.add_cog(e(client))
+    await client.add_cog(evaluate(client))
