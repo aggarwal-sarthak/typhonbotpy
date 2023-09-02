2a454cec56492cfdcfbaec6e90e30e8848a4ce51
ShubhamPokhariyal <shubhampokhariyal2@gmail.com>
2023-09-02 16:06:42 +0530

    modified:   cogs/Information/help.py

--- cogs/Information/help.py
+++ cogs/Information/help.py
@@ -13,24 +13,26 @@ class help(commands.Cog):
     @commands.bot_has_permissions(embed_links=True)
     @commands.command(description='Returns Help Menu For The Bot Commands', usage=f"{os.path.basename(__file__)[:-3]} [command]")
     async def help(self,ctx,*arg):
+        
+        #Fetching prefix
+        res = self.client.db.guilds.find_one({"guild_id": ctx.guild.id})
+        if res and "prefix" in res:
+            prefix = res['prefix']
+        else:
+            prefix = self.client.config['prefix']
         if(len(arg)):
-            cmd = self.client.get_cog(arg[0].lower()).get_commands()
-            menu = discord.Embed(title="Command Details",color=0xfb7c04, description=f"```- [] = Optional Arguments\n- <> = Required Arguments\n- Do Not Type These When Using Commands!```\n> {[c.description for c in cmd][0]}")
-            if [c.aliases for c in cmd][0]:
-                aliases = ['`, `'.join(c.aliases) for c in cmd][0]
+            cmd = self.client.get_command(arg[0].lower())
+            menu = discord.Embed(title="Command Details",color=0xfb7c04, description=f"```- [] = Optional Arguments\n- <> = Required Arguments\n- Do Not Type These When Using Commands!```\n> {cmd.description}")
+            if (cmd.aliases):
+                aliases = '`, `'.join([c for c in cmd.aliases])
             else:
                 aliases = "None"
             menu.add_field(name="Aliases", value=f"`{aliases}`")
-            usage = (self.client.config["prefix"]+[c.usage for c in cmd][0]).replace('\n',f'\n{self.client.config["prefix"]}')
-            menu.add_field(name="Usage", value=f'`{usage}`')
+            usage = (prefix+cmd.usage).replace('\n',f'\n{prefix}')
+            menu.add_field(name="Usage", value=f'`{usage}`',inline=False)
+            menu.add_field(name="Cooldown", value=f'`{str(int(cmd.cooldown.per))+"s" if cmd.cooldown is not None else "No Cooldown!"}`',inline=False)
             await ctx.reply(embed=menu)
         else:
-            res = self.client.db.guilds.find_one({"guild_id": ctx.guild.id})
-            print(res)
-            if res and "prefix" in res:
-                prefix = res['prefix']
-            else:
-                prefix = self.client.config['prefix']
             embed = discord.Embed(title="TYPHON BOT Commands",color=0xfb7c04)
             embed.add_field(name="",value=f"{self.client.emotes['success']} : Prefix For This Server : `{prefix}`\n\
                             {self.client.emotes['success']} : Total Bot Commands : `{len(self.client.commands)}`\n\
@@ -39,6 +41,7 @@ class help(commands.Cog):
                              | [Support Server](https://discord.com/invite/5UqVvZj)\
                              | [Vote For Typhon Bot](https://top.gg/bot/756052319417925633/vote)",inline=False)
             for folder in os.listdir('./cogs'):
+                if(folder=="Developer"): continue
                 embed.add_field(name=f"{folder} Commands",value=f"`{'`, `'.join([filename[:-3] for filename in os.listdir(f'./cogs/{folder}') if filename.endswith('.py')])}`",inline=False)
             embed.set_thumbnail(url=self.client.user.display_avatar)
             embed.set_footer(text=f"Requested by {ctx.author}",icon_url=ctx.author.avatar)
