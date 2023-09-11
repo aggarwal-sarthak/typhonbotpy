e5e74a53068a0ed2ac4904d54e630e1e36a8544d
Sarthak Aggarwal <70897806+aggarwal-sarthak@users.noreply.github.com>
2023-09-11 18:56:44 +0530

    Update purge.py

--- cogs/Moderation/purge.py
+++ cogs/Moderation/purge.py
@@ -1,7 +1,5 @@
 from discord.ext import commands
 import os
-from types import SimpleNamespace
-import asyncio
 from validation import is_command_enabled
 
 class purge(commands.Cog):
@@ -12,49 +10,55 @@ class purge(commands.Cog):
     async def on_ready(self):
         print(f"✅ | {os.path.basename(__file__)[:-3]} Is Loaded!")
 
-    @commands.command(description='Clears Messages For Given Parameters', usage=f"{os.path.basename(__file__)[:-3]} <amount>\n{os.path.basename(__file__)[:-3]} <Member> <amount>\n{os.path.basename(__file__)[:-3]} bots <amount>\n{os.path.basename(__file__)[:-3]} embeds <amount>\n{os.path.basename(__file__)[:-3]} humans <amount>\n{os.path.basename(__file__)[:-3]} images <amount>\n{os.path.basename(__file__)[:-3]} images <amount>")
+    @commands.group(name='purge', description='Clears Messages For Given Parameters', aliases = ['clear'], invoke_without_command=True)
     @commands.check(is_command_enabled)
     @commands.has_permissions(manage_messages=True)
     @commands.bot_has_permissions(manage_messages=True)
-    async def purge(self, ctx, *args):
-        mode = [str(x) for x in args if not x.isdigit()][0] if len([str(x) for x in args if not x.isdigit()]) else []
-        amount = [int(x) for x in args if x.isdigit()][0] if len([int(x) for x in args if x.isdigit()]) else 50
-        if amount <=0: raise commands.MissingRequiredArgument(SimpleNamespace(displayed_name="amount"))
-        await ctx.message.delete()
-        if len(mode) and not (mode.startswith("<@") or mode.endswith(">")):
-            match mode[0]:
-                case "bots" | "bot" | "b":
-                    def check(message):
-                        asyncio.sleep(0.5)
-                        return not message.pinned and message.author.bot
-                    
-                case "embeds" | "embed" | "e":
-                    def check(message):
-                        asyncio.sleep(0.5)
-                        return not message.pinned and message.embeds
+    async def purge(self, ctx, amount: int):
+        def check(message):
+            return not message.pinned
+        await delete(self, ctx, amount, check)
 
-                case "humans" | "human" | "h":
-                    def check(message):
-                        asyncio.sleep(0.5)
-                        return not message.pinned and not message.author.bot
-                    
-                case "images" | "image" | "i":
-                    def check(message):
-                        asyncio.sleep(0.5)
-                        return not message.pinned and (message.attachments or ('.jpg' or '.jpeg' or '.png' or '.webp') in message.content)
-                    
-                case _:
-                    raise commands.MissingRequiredArgument(SimpleNamespace(displayed_name="mode"))
-        else:
-            if mode.startswith("<@") and mode.endswith(">"):
-                def check(message):
-                    return not message.pinned and str(message.author.id) == str(mode[mode.index("<@")+2:mode.index(">")])
-            else:
-                def check(message):
-                    return not message.pinned
-                
-        msgs = await ctx.channel.purge(limit=amount, check=check)
-        await ctx.channel.send(f'{self.client.emotes["success"]} | {len(msgs)} Messages Deleted Successfully!')
+    @purge.command(name='bots', description='Clears Messages For Bots', aliases = ['bot', 'b'], usage = f"{os.path.basename(__file__)[:-3]} bots <amount>")
+    @commands.check(is_command_enabled)
+    @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
+    async def bots(self, ctx, amount: int):
+        def check(message):
+            return not message.pinned and message.author.bot
+        await delete(self, ctx, amount, check)
+
+    @purge.command(name='humans', description='Clears Messages For Humans', aliases = ['human', 'h'], usage = f"{os.path.basename(__file__)[:-3]} humans <amount>")
+    @commands.check(is_command_enabled)
+    @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
+    async def bots(self, ctx, amount: int):
+        def check(message):
+            return not message.pinned and not message.author.bot
+        await delete(self, ctx, amount, check)
+
+    @purge.command(name='embeds', description='Clears Embeds', aliases = ['embed', 'e'], usage = f"{os.path.basename(__file__)[:-3]} embeds <amount>")
+    @commands.check(is_command_enabled)
+    @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
+    async def bots(self, ctx, amount: int):
+        def check(message):
+            return not message.pinned and message.embeds
+        await delete(self, ctx, amount, check)
+
+    @purge.command(name='images', description='Clears Images', aliases = ['image', 'img', 'imgs'], usage = f"{os.path.basename(__file__)[:-3]} images <amount>")
+    @commands.check(is_command_enabled)
+    @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
+    async def bots(self, ctx, amount: int):
+        def check(message):
+            return not message.pinned and (message.attachments or ('.jpg' or '.jpeg' or '.png' or '.webp') in message.content)
+        await delete(self, ctx, amount, check)
+        
+async def delete(self, ctx, amount, check):
+    await ctx.message.delete()
+    msgs = await ctx.channel.purge(limit=amount, check=check)
+    await ctx.channel.send(f'{self.client.emotes["success"]} | {len(msgs)} Messages Deleted Successfully!')
 
 async def setup(client):
     await client.add_cog(purge(client))
