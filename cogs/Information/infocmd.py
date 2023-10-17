76b64740691c0a6b674ffe4829387c551beb0918
ShubhamPokhariyal <shubhampokhariyal2@gmail.com>
2023-10-17 22:20:49 +0530

    fixed info command

--- cogs/Information/infocmd.py
+++ cogs/Information/infocmd.py
@@ -7,6 +7,7 @@ import datetime
 class infocmd(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.start_time = None
 
     @commands.command(description='Returns Help Menu For The Bot Commands', usage=f"{os.path.basename(__file__)[:-3]} [command]")
     @commands.check(is_command_enabled)
@@ -55,8 +56,8 @@ class infocmd(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        global start_time
-        start_time = datetime.datetime.now()
+        self.start_time = datetime.datetime.now()
+
 
     @commands.command(description='Returns Information About Bot', aliases=['botinfo'], usage=f"{os.path.basename(__file__)[:-3]}")
     @commands.bot_has_permissions(embed_links=True)
@@ -67,7 +68,7 @@ class infocmd(commands.Cog):
         embed.add_field(name="Owner",value=f"[{await self.client.fetch_user(self.client.config['owner'][0])}](https://discord.com/users/{self.client.config['owner'][0]}) [<@!{self.client.config['owner'][0]}>]\n[{await self.client.fetch_user(self.client.config['owner'][1])}](https://discord.com/users/{self.client.config['owner'][1]}) [<@!{self.client.config['owner'][1]}>]",inline=False)
         embed.add_field(name="Total Servers",value=len(self.client.guilds),inline=False)
         embed.add_field(name="Total Users",value=sum(len(guild.members) for guild in self.client.guilds),inline=False)
-        td = datetime.datetime.now() - start_time
+        td = datetime.datetime.now() - self.start_time
         days = td.days
         hours, remainder = divmod(td.seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
